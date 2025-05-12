a9e0e6f0d635b4ea65842362c8eb81d88555f268
juancc9 <sicsound00@gmail.com>
2025-05-11 20:56:20 -0500

    "Cargar fotos"

---
+++ frontend/js/JJM_adiccionar.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btnSubirFoto");
+  const input = document.getElementById("foto");
+  const textoPorDefecto = "Subir una foto";
+
+  btn.textContent = textoPorDefecto ;
+
+  btn.addEventListener("click", () => {
+    input.click();
+  });
+
+  input.addEventListener("change", () => {
+    if (input.files.length > 0) {
+      btn.textContent = ` ${input.files[0].name}`;
+    } else {
+      btn.textContent = textoPorDefecto + " Ninguna";
+    }
+  });
+});
